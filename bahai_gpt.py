dc7712c04e829b478290fa3be3a4e8100c77de03
Shoghi Najaf-Toomraei <shoghn.toomraei19@gmail.com>
2025-06-06 16:26:40 -0400

    Fixing welcome

--- bahai_gpt.py
+++ bahai_gpt.py
@@ -49,7 +49,7 @@ if prompt:=st.chat_input("Type your question here"):
         st.write(prompt)#maybe use .markdown() insetad
     st.session_state.history.append({"role":"user","content":prompt})#maybe use .append() instead of +=
     #B-GPT
-    with st.chat_message("assistant"):
+    with st.chat_message(name="assistant",avatar="📜"):
         message_holder=st.empty()
         full_response=""
         response = st.session_state.chat.send_message_stream(prompt,config=generate_content_config)
