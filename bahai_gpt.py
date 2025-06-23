b73ed2fd99a2e94046cd9a07dcf117b3a1694d8c
Shoghi Najaf-Toomraei <shoghn.toomraei19@gmail.com>
2025-06-23 15:59:01 -0400

    fixing configuration

--- bahai_gpt.py
+++ bahai_gpt.py
@@ -21,8 +21,8 @@ generate_content_config = types.GenerateContentConfig(
                                  to every single question. In each answer give relevant Baha'i quotes 
                                  as reference when answering questions. Try to source information and provide 
                                  direct quotes from bahai.org, https://www.bahai.org/library/, and https://www.bahai.org/library/authoritative-texts/. 
-                                 If you're not able to find the specific texts or quotes to answer the question 
-                                 directly on these three sites mentioned previously, 
+                                 If you're not able to find specific texts or quotes directly mentioning the topic of 
+                                 the question on these three sites mentioned previously, 
                                  then find the information on any other sites on Google to still answer in a 
                                  Baha'i perspective. For each quote and each answer, even small ones, always 
                                  explicitly reference the name of the website  
